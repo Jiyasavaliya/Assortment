cc3b43614318e6d94221a6463370894008478830
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-03-20 01:04:15 +0530

    Add files via upload

---
+++ Assortment colam.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+int main(){
+	int n;
+	
+    printf("Enter Value:");
+	scanf("%d",&n);
+
+	int arr[n][n];
+int i,j;	
+	for(i=0; i<n; i++){
+		
+		for(j=0; j<n; j++){
+			
+			printf("Enter elements [%d][%d]:",i,j);
+			scanf("%d",&arr[i][j]);
+	
+		
+}}
+	for(i=0; i<n; i++){
+		
+		for(j=0; j<n; j++){
+				printf("%d",arr[i][j]);
+			}
+			
+			printf("\n");
+			return 0;
+		}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
---
+++ Assortment negative elements.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+main(){
+	
+	int n;
+	
+	printf("Enter Number: ");
+	scanf("%d",&n);
+	
+	int i;
+	int a[n] ;
+	for(i=0; i<n; i++){
+		
+		
+		scanf("%d",&a[i]);
+	}
+	  
+	
+	for(i=0; i<n; i++){
+		if(a[i]<0){
+			
+			
+		}
+		
+			printf("%d\n",a[i]);
+		
+	
+	}
+	
+	
+	
+}
